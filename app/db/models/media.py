8cae84d9d5cfafbdeaeea49a7f8a7646c5983eb2
keanutaufan <keanu.taufan21@gmail.com>
2024-03-06 21:10:47 +0700

    Add uploader id to media model

--- app/db/models/media.py
+++ app/db/models/media.py
@@ -6,3 +6,4 @@ from .base import UUIDModel, TimestampModel
 class MediaModel(UUIDModel, TimestampModel, table=True):
     __tablename__ = "media"
     extension: str
+    uploader_id: uuid_pkg.UUID = Field(foreign_key="users.id")
